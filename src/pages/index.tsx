9f859cc4e99a9f32a74ed9a711a90e8259403e8a
Axle Rollon <grammerpro86@gmail.com>
2023-06-09 09:50:13 +0800

    New files for new routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { SignInButton, SignedIn, SignedOut, useUser } from "@clerk/nextjs";
 
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import type { NextPage } from "next";
 import type { RouterOutputs } from "~/utils/api";
 import { api } from "~/utils/api";
@@ -93,10 +94,14 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username}`}</span>
-          <span className="font-thin">
-            {` · ${dayjs(post.createdAt).fromNow()}`}
-          </span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">
+              {` · ${dayjs(post.createdAt).fromNow()}`}
+            </span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
