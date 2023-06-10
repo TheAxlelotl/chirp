072dc65bd3ab9068a37cf6c532e1a8ce0117e88f
Axle Rollon <grammerpro86@gmail.com>
2023-06-10 11:31:08 +0800

    Profile page with SSG helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,19 @@ import "~/styles/globals.css";
 
 import type { AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 import { api } from "~/utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Toaster position="bottom-center"/>
+      <Head>
+        <title>Chirp</title>
+        <meta name="description" content="💭" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
   );
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -7,29 +7,15 @@ import {
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
 import { TRPCError } from "@trpc/server";
-import type { User } from "@clerk/nextjs/dist/types/server";
 import { clerkClient } from "@clerk/nextjs";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 import { z } from "zod";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(3, "1 m"),
   analytics: true,
-  /**
-   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
-   * instance with other applications and want to avoid key collisions. The default prefix is
-   * "@upstash/ratelimit"
-   */
-  prefix: "@upstash/ratelimit",
 });
 
 export const postsRouter = createTRPCRouter({
